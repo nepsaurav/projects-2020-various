5ba2b422eead00dc77e9d132efc5c3cda07dff42
Saurav Gautam <sgaut001@plattsburgh.edu>
2020-12-05 23:44:24 -0500

    First commit
    - Testing linq ✔️
    - Converting a string array to int array ✔️

---
+++ tinker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace tinker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] nums = new string[]{"3671", "23123", "123123"};
+            var linqTest = nums.Select(y => y.First());
+            Console.WriteLine("First Test");
+            Console.WriteLine(linqTest);
+            
+            int[] digits = new int[3];
+
+            for(int i = 0; i < nums.Length; i++){
+                int val = Int32.Parse(nums[i]);
+                digits[i] = val;
+            }
+            Console.WriteLine("Second Test");
+            foreach(int item in digits){
+                Console.WriteLine("{0} ", item);
+            }
+        }
+    }
+}
